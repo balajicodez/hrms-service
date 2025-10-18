4416303b544cacfaba6d5f7e059804082aa1f0d8
balajicodez <160614779+balajicodez@users.noreply.github.com>
2025-10-18 21:21:21 +0530

    employee

---
+++ src/main/java/com/simplerp/hrms/config/RestCorsConfig.java
@@ -0,0 +1,16 @@
+package com.simplerp.hrms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestCorsConfig implements RepositoryRestConfigurer {
+
+    public void configureCorsRegistry(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+
+}
