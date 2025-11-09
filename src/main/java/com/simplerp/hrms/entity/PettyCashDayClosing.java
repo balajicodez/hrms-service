08fd013036bd0fd2a5ec47cd8b9dcc5263981e7d
balajicodez <160614779+balajicodez@users.noreply.github.com>
2025-11-09 20:38:24 +0530

    dayclosing

--- src/main/java/com/simplerp/hrms/entity/PettyCashDayClosing.java
+++ src/main/java/com/simplerp/hrms/entity/PettyCashDayClosing.java
@@ -1,13 +1,20 @@
 package com.simplerp.hrms.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class PettyCashDayClosing {
 
